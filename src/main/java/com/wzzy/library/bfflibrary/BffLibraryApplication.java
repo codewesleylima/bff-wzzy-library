c5aed9bf9cf14be1efd1c137d56ae117aed2e881
codewesleylima <gusterlima@hotmail.com>
2026-05-24 19:08:16 -0300

    first commit

---
+++ src/main/java/com/wzzy/library/bfflibrary/BffLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.wzzy.library.bfflibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BffLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BffLibraryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wzzy/library/bfflibrary/BffLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wzzy.library.bfflibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffLibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
