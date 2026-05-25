624589e3c54082fce1a484817ec401c4c48e7ae1
codewesleylima <gusterlima@hotmail.com>
2026-05-24 23:37:24 -0300

    feat(bff-library): add ApiResponse DTO

---
+++ src/main/java/com/wzzy/library/bfflibrary/dto/ApiResponse.java
@@ -0,0 +1,18 @@
+package com.wzzy.library.bfflibrary.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiResponse<T> {
+    private boolean success;
+    private String message;
+    private T data;
+    private String timestamp;
+    private String path;
+}
